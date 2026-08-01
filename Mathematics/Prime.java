ea396fafb95c3e471e7e76a4683f48c7ba7fa12f
Manabendu-ai <rikugithub@gmail.com>
2026-08-01 23:35:29 +0530

    Program7: Prime

---
+++ Mathematics/Prime.java
@@ -0,0 +1,30 @@
+package Mathematics;
+
+public class Prime {
+    public static boolean isPrime(int n){
+        if(n<=1){
+            return false;
+        }
+        if(n==2 || n==3){
+            return true;
+        }
+        if(n%2==0 || n%3==0){
+            return false;
+        }
+        for(int i=5; i*i<=n; i = i + 6){
+            if (n%i==0 || n%(i+1)==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int n = 7; 
+      	if(isPrime(n)){
+        	System.out.println("true");
+        }else{
+          System.out.println("false");
+    	}
+    }
+}
