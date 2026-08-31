e778bfdc61134ce3a9e5fa7ce1192fec8d3fe548
Manabendu-ai <rikugithub@gmail.com>
2026-08-31 20:01:34 +0530

    Log base 2

--- Recursion/LogBase2.java
+++ Recursion/LogBase2.java
@@ -1,5 +1,7 @@
 package Recursion;
-
+/*
+It computes the log base 2 n for the powers of 2 and for non 2 powers it return the floor of the log base 2 n
+ */
 public class LogBase2 {
 
     public static int fun(int n){
