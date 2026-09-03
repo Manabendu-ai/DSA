07c0f2e0256c742fe3d4aa6272670a60bb194942
Manabendu-ai <rikugithub@gmail.com>
2026-09-03 20:47:44 +0530

    JosephusProblem --detail

--- Recursion/JosephusProblem.java
+++ Recursion/JosephusProblem.java
@@ -1,5 +1,14 @@
 package Recursion;
 
+/**
+
+ This is a very interesting problem:
+ n friends are standing in a circle and let's say the person at 0th position has the gun first.
+ Now that person must shoot the kth person(including him).
+ After shoot the kth person the person next to kth person will get the gun,
+ This repeats until there a single person survives till the end who is the winner!
+
+ */
 public class JosephusProblem {
     public static int jos(int n, int k){
         if(n == 1){
